acb22066a30f490cdecba9f8a8350d26e22b076c
Alex Glaser <119404263+glaser-alex@users.noreply.github.com>
2023-07-09 15:49:06 +0200

    file upload

--- chat/auswerten.php
+++ chat/auswerten.php
@@ -1,6 +1,7 @@
 <?php
     session_start();
     // $_POST = array_map('htmlspecialchars', $_POST);
+    // echo "<pre>".print_r($_POST)."</pre>";
     $username = @$_SESSION['username'];
 
     if ($_SESSION['username'] == 'admin') {
@@ -10,19 +11,74 @@
     }
     
     $dateiname = "./chat.txt";
-    if ($_POST['gesendet']) {
-        $datei = fopen($dateiname, "a");
-        flock($datei, LOCK_EX);
+    if ($_POST['submit']) {
 
         $date = date('(H:i) ');
-        if (filesize($dateiname) < 3) {
-            fputs($datei, "<span style='font-size: x-small'>".$date."</span>".$username." ".$_POST['message']."\n\n");
-        } else {
-            fputs($datei, "<br><span style='font-size: x-small'>".$date."</span>".$username." ".$_POST['message']."\n\n");
-        }
+        $datei = fopen($dateiname, "a");
 
+        if (isset($_FILES["fileToUpload"]["name"])) {
+
+            $target_dir = "uploads/";
+            $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+            $uploadOk = 1;
+            $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+            
+            // Check if image file is a actual image or fake image
+            if(isset($_POST["submit"])) {
+            $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+            if($check !== false) {
+                echo "File is an image - " . $check["mime"] . ".";
+                $uploadOk = 1;
+            } else {
+                echo "File is not an image.";
+                $uploadOk = 0;
+            }
+            }
+            
+            // Check file size
+            if ($_FILES["fileToUpload"]["size"] > 500000) {
+            echo "Sorry, your file is too large.";
+            $uploadOk = 0;
+            }
+            
+            // Allow certain file formats
+            if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+            && $imageFileType != "gif" ) {
+            echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+            $uploadOk = 0;
+            }
+            
+            // Check if $uploadOk is set to 0 by an error
+            if ($uploadOk == 0) {
+            echo "Sorry, your file was not uploaded.";
+            // if everything is ok, try to upload file
+            } else {
+                if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+                    flock($datei, LOCK_EX);
+                    if (filesize($dateiname) < 3) {
+                        fputs($datei, "<span style='font-size: x-small'>".$date."</span><img src='./uploads/".$_FILES['fileToUpload']['name']."' width='150'>".$_POST['message']."\n\n");
+                    } else {
+                        fputs($datei, "<br><span style='font-size: x-small'>".$date."</span><img src='./uploads/".$_FILES['fileToUpload']['name']."' width='150'>".$_POST['message']."\n\n");
+                    }
+                    echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
+                } else {
+                    echo "Sorry, there was an error uploading your file.";
+                }
+            }            
+        } else {
+            
+            flock($datei, LOCK_EX);
+            if (filesize($dateiname) < 3) {
+                fputs($datei, "<span style='font-size: x-small'>".$date."</span>".$username." ".$_POST['message']."\n\n");
+            } else {
+                fputs($datei, "<br><span style='font-size: x-small'>".$date."</span>".$username." ".$_POST['message']."\n\n");
+            }
+            
+        }
+        
         flock($datei, LOCK_UN);
         fclose($datei);
+
         header("Location: ./chat.php");
     } else {
         header("Location: ./chat.php");
--- chat/chat.php
+++ chat/chat.php
@@ -5,8 +5,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./style.css">
-    <!-- <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script> -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3/jquery.min.js"></script>
     <title>Chat</title>
 </head>
@@ -20,28 +18,31 @@
     ?>
   </div>
 </div>
-<form action="./auswerten.php" method="post">
+<div class="form">
+  <form action="./auswerten.php" method="post" enctype="multipart/form-data">
+    <input type="file" name="fileToUpload" id="fileToUpload">
     <input type="text" name="message" placeholder="Sag dem alex wie gern du ihn hast" required autofocus>
-    <input type="submit" name="gesendet" value="Senden">
-    <div class='form-switch' style='width: 200%;'>
-      <input class='form-check-input' type='checkbox' role='switch' id='flexSwitchCheckChecked' onclick='Autoscroll()' checked>
-      Autoscroll <span id='on-off'>OFF</span>
-    </div>
-</form>
+    <input type="submit" name="submit" value="Senden">
+  </form>
+  <br>
+  <button onclick="Autoscroll()">Autoscroll <span id='on-off'>OFF</span></button>
+</div>
 </body>
 </html>
 <script type="text/javascript">
 
-function Autoscroll() {
-   var x = document.getElementById("on-off");
-		if (x.innerHTML === "ON") {
-            x.innerHTML = "OFF";
-            setInterval(scrollToBottom, -1);
-            window.location.reload();
-          } else {
-            x.innerHTML = "ON";
-            setInterval(scrollToBottom, 0);
-		}
+// setInterval(scrollToBottom, 0);
+
+function Autoscroll(submit) {
+  var x = document.getElementById("on-off");
+    if (x.innerHTML === "OFF") {
+      x.innerHTML = "ON";
+      setInterval(scrollToBottom, 0);
+    } else if (x.innerHTML === 'ON') {
+      x.innerHTML = "OFF";
+      setInterval(scrollToBottom, -1);
+      window.location.reload();
+    }
 }
 
 function scrollToBottom() {
